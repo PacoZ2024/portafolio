314783df2ce005c7e65742cb5d356e2119cfec96
Megaman <pacosclub@gmail.com>
2026-07-10 18:15:39 -0600

    fix(Footer): Se ajusta amplia el espacio debajo del footer para visualizar el correo electrónico en dispositivos móviles una vez que seleccionamos Contacto en el encabezado.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -104,7 +104,6 @@ export default function Navigation() {
                 to='footer'
                 smooth={true}
                 duration={500}
-                offset={100}
                 onClick={closeToggled}
               >
                 Contacto
